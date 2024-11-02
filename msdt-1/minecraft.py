d21a92b87542ceaa7185990795f1ccf9e96fa838
nikipa <nikipagmal@gmail.com>
2024-11-02 15:33:32 +0400

    fixing whitespaces in expressions and statements

--- msdt-1/minecraft.py
+++ msdt-1/minecraft.py
@@ -4,84 +4,84 @@ import os
 import shutil
 
 
-t=0
-x1=16
-y1=16
-x2=16
-y2=16
-x3=16
-y3=16
+t = 0
+x1 = 16
+y1 = 16
+x2 = 16
+y2 = 16
+x3 = 16
+y3 = 16
 
 
-def getblock(x,y,z):
+def getblock(x, y, z):
     chunk = anvil.Chunk.from_region(region, x//16, z//16)
     block = chunk.get_block(x%16, y, z%16)
     return block.id
 
 
-def addframe(g,ems):
+def addframe(g, ems):
     global t
     func = open('C:/Users/nikipa/AppData/Roaming/.tlauncher/legacy/Minecraft/game/saves/qs (1)/datapacks/build/data/minecraft/functions/pf' + str(t) + '.mcfunction', "w")
     for q in g:
         func.write('setblock ' + str(q[0]) + ' ' + str(q[1]) + ' ' + str(q[2]) + ' netherrack' + '\n')
     for q in ems:
         func.write('setblock ' + str(q[0]) + ' ' + str(q[1]) + ' ' + str(q[2]) + ' emerald_block' + '\n')
-    func.write('schedule function pf'+str(t+1)+' 0.2s')
-    t+=1
+    func.write('schedule function pf' + str(t+1) + ' 0.2s')
+    t += 1
 
 
-def bigfunc(x,y,c):
-    if c!=2:
-        return 0.5<=math.e**(-0.06*((x-x1)**2+(y-y1)**2))+math.e**(-0.06*((x-x2)**2+(y-y2)**2))+math.e**(-0.06*((x-x3)**2+(y-y3)**2))
+def bigfunc(x, y, c):
+    if c != 2:
+        return 0.5  <=  math.e ** (-0.06 * ((x-x1)**2 + (y-y1)**2))  +  math.e ** (-0.06 * ((x-x2)**2 + (y-y2)**2))  +  math.e ** (-0.06 * ((x-x3)**2 + (y-y3)**2))
     else:
-        return 0.5<=math.e**(-0.06*((x-x1)**2+(y-y1)**2))+math.e**(-0.06*((x-x2)**2+(y-y2)**2))+math.e**(-0.06*((x-x3)**2+(y-y3)**2))+math.e**(y-24)
+        return 0.5  <=  math.e ** (-0.06 * ((x-x1)**2 + (y-y1)**2))  +  math.e ** (-0.06 * ((x-x2)**2 + (y-y2)**2))  +  math.e ** (-0.06 * ((x-x3)**2 + (y-y3)**2))  +  math.e ** (y-24)
 
 
 def addframe(c):
     global t
-    print(t,x1,y1,x2,y2,x3,y3)
+    print(t, x1, y1, x2, y2, x3, y3)
     func = open('C:/Users/nikipa/AppData/Roaming/.tlauncher/legacy/Minecraft/game/saves/qs (1)/datapacks/build/data/minecraft/functions/pf' + str(t) + '.mcfunction', "w")
-    func.write('fill 0 0 0 63 63 0 '+('white' if c!=2 else 'purple')+'_concrete\n')
+    func.write('fill 0 0 0 63 63 0 ' + ('white' if c!=2 else 'purple') + '_concrete\n')
     for x in range(64):
         for y in range(64):
-            if(bigfunc((x+0.5)/2,(y+0.5)/2,c)):
-                func.write('setblock '+str(x)+' '+str(y)+' 0 '+('blue'if c==0 else 'lime' if c==1 else 'white')+'_concrete\n')
-    func.write('schedule function pf' + str(t + 1) + ' 1')
+            if(bigfunc( (x+0.5)/2, (y+0.5)/2, c )):
+                func.write('setblock ' + str(x) + ' ' + str(y) + ' 0 ' + ('blue'if c==0 else 'lime' if c==1 else 'white') + '_concrete\n')
+    func.write('schedule function pf' + str(t+1) + ' 1')
 
 
 def grow():
     global sorces
     global paths
     builded = []
-    while len(sorces)>0:
+    while len(sorces) > 0:
         growcords = []
         for sorce in sorces:
             begx = sorce[0]
             begy = sorce[1]
             begz = sorce[2]
-            q = (begx + 1, begy, begz)
+            q = (begx+1, begy, begz)
             if q not in growcords:
                 growcords.append(q)
-            q = (begx-1,begy,begz)
+            q = (begx-1, begy, begz)
             if q not in growcords:
                 growcords.append(q)
-            q = (begx,begy+1,begz)
+            q = (begx, begy+1, begz)
             if q not in growcords:
                 growcords.append(q)
-            q = (begx,begy-1,begz)
+            q = (begx, begy-1, begz)
             if q not in growcords:
                 growcords.append(q)
-            q = (begx,begy,begz+1)
+            q = (begx, begy, begz+1)
             if q not in growcords:
                 growcords.append(q)
-            q = (begx,begy,begz-1)
+            q = (begx, begy, begz-1)
             if q not in growcords:
                 growcords.append(q)
-        print(t,'start',len(paths),len(builded))
-        q=[x for x in growcords if x not in paths and x not in builded]
-        print(t,'finish')
-        addframe(sorces,q)
-        builded+=sorces
+        print(t, 'start', len(paths), len(builded))
+        q = [x for x in growcords if x not in paths and x not in builded]
+        print(t, 'finish')
+        addframe(sorces, q)
+        builded += sorces
         sorces=q
 
 
@@ -89,43 +89,43 @@ region = anvil.Region.from_file('C:/Users/nikipa/AppData/Roaming/.tlauncher/lega
 path = 'C:/Users/nikipa/AppData/Roaming/.tlauncher/legacy/Minecraft/game/saves/qs (1)/datapacks/build/data/minecraft/functions/'
 shutil.rmtree(path)
 os.makedirs(path)
-c=0
+c = 0
 
 
 for t in range(20):
     addframe(c)
-for t in range(20*1,20//2*13):
-    if t ==20*3:
-        c=1
-    if t>=20*1 and t<20*2:
-        y1=5*(1-math.cos(math.pi*(t/20-1)))+16
-    if t>=20*1.5 and t<20*2.5:
-        x2=5*(1-math.cos(math.pi*(t/20-1.5)))+16
-        y2=-5*(1-math.cos(math.pi*(t/20-1.5)))+16
-    if t>=20*2 and t<20*3:
-        x3=-5*(1-math.cos(math.pi*(t/20-2)))+16
-        y3=-5*(1-math.cos(math.pi*(t/20-2)))+16
-    if t>=20*3.5 and t<20*4.5:
-        x2 = -5 * (1 - math.cos(math.pi * (t / 20 - 3.5))) + 26
-        x3 = 5 * (1 - math.cos(math.pi * (t / 20 - 3.5))) + 6
-    if t>=20*4.5 and t<20*5.5:
-        y2=5*(1-math.cos(math.pi*(t/20-4.5)))+6
-        y3=5*(1-math.cos(math.pi*(t/20-4.5)))+6
-    if t>=20*5 and t<20*6:
-        y1=-5*(1-math.cos(math.pi*(t/20-5)))+26
+for t in range(20*1, 20//2*13):
+    if t == 20 * 3:
+        c = 1
+    if t >= 20*1 and t < 20*2:
+        y1 = 5 * (1 - math.cos(math.pi * (t/20-1))) + 16
+    if t >= 20*1.5 and t < 20*2.5:
+        x2 = 5 * (1 - math.cos(math.pi * (t/20-1.5))) + 16
+        y2 = -5 * (1 - math.cos(math.pi * (t/20-1.5))) + 16
+    if t >= 20*2 and t < 20*3:
+        x3 =- 5 * (1 - math.cos(math.pi * (t/20-2))) + 16
+        y3 =- 5 * (1 - math.cos(math.pi * (t/20-2))) + 16
+    if t >= 20*3.5 and t < 20*4.5:
+        x2 = -5 * (1 - math.cos(math.pi * (t/20-3.5))) + 26
+        x3 = 5 * (1 - math.cos(math.pi * (t/20-3.5))) + 6
+    if t >= 20*4.5 and t < 20*5.5:
+        y2 = 5 * (1 - math.cos(math.pi * (t/20-4.5))) + 6
+        y3 = 5 * (1 - math.cos(math.pi * (t/20-4.5))) + 6
+    if t >= 20*5 and t < 20*6:
+        y1 = -5 * (1 - math.cos(math.pi * (t/20-5))) + 26
     addframe(c)
 
 
-c=2
-boba=2
-x1=14+boba
-x2=23+boba
-x3=5+boba
+c = 2
+boba = 2
+x1 = 14 + boba
+x2 = 23 + boba
+x3 = 5 + boba
 
 
-for t in range(20*6,20*6+20//2*5+1):
-    td=(t-20*6)*7/50+1
-    y1=32-td**2
-    y2=32-(td-1)**2
-    y3=32-(td-2)**2
+for t in range(20*6, 20*6 + 20//2*5 + 1):
+    td = (t-20*6) * 7 / 50 + 1
+    y1 = 32 - td**2
+    y2 = 32 - (td-1)**2
+    y3 = 32 - (td-2)**2
     addframe(c)
--- msdt-1/triangles.py
+++ msdt-1/triangles.py
@@ -1,22 +1,22 @@
 import pygame
 
 
-dots=[]
-lines=[]
-side=90
+dots = []
+lines = []
+side = 90
 clock = pygame.time.Clock()
-mousepos=(0,0)
+mousepos = (0, 0)
 
 
 def dotsgen():
     for y in range(13):
         for x in range(25):
-            dots.append((int(x*side*(3**0.5)/2),y*side+(x%2==0)*side//2))
+            dots.append((int( x * side * (3**0.5) / 2 ), y*side + (x%2==0)*side//2 ))
 
 
 def dotsdraw():
     for dot in dots:
-        pygame.draw.circle(screen,(200,0,0),dot,10)
+        pygame.draw.circle(screen, (200, 0, 0), dot, 10)
 
 
 def linesdraw():
@@ -24,50 +24,50 @@ def linesdraw():
         pygame.draw.line(screen, (100, 100, 100), line[0], line[1], 5)
 
 
-def rast(d1,d2):
-    return ((d1[0]-d2[0])**2+(d1[1]-d2[1])**2)**0.5
+def rast(d1, d2):
+    return ( (d1[0]-d2[0])**2 + (d1[1]-d2[1])**2 )**0.5
 
 
-def corrpos(pos,begpos):
-    mindot=dots[0]
-    minrast=rast(pos,dots[0])
+def corrpos(pos, begpos):
+    mindot = dots[0]
+    minrast = rast(pos, dots[0])
     for dot in dots:
-        if rast(pos,dot)<minrast:
-            mindot=dot
-            minrast=rast(pos,dot)
+        if rast(pos, dot) < minrast:
+            mindot = dot
+            minrast = rast(pos, dot)
     return mindot
 
 
 def corrpos(pos):
-    mindot=dots[0]
-    minrast=rast(pos,dots[0])
+    mindot = dots[0]
+    minrast = rast(pos, dots[0])
     for dot in dots:
-        if rast(pos,dot)<minrast:
-            mindot=dot
-            minrast=rast(pos,dot)
+        if rast(pos, dot) < minrast:
+            mindot = dot
+            minrast = rast(pos, dot)
     return mindot
 
 
-def linedotrast(dot,line):
-    a=rast(dot,line[0])
-    b=rast(dot,line[1])
-    c=rast(line[0],line[1])
-    h=(((a+b+c)*(a+b-c)*(a-b+c)*(-a+b+c))**0.5/4)*2//c
-    am = (a ** 2 - h ** 2) ** 0.5
-    bm = (b ** 2 - h ** 2) ** 0.5
-    if am<=c and bm<=c:
+def linedotrast(dot, line):
+    a = rast(dot, line[0])
+    b = rast(dot, line[1])
+    c = rast(line[0], line[1])
+    h = (((a+b+c) * (a+b-c) * (a-b+c) * (-a+b+c)) ** 0.5 / 4) * 2 // c
+    am = (a**2 - h**2) ** 0.5
+    bm = (b**2 - h**2) ** 0.5
+    if am <= c and bm <= c:
         return h
     else:
-        return min(am,bm)
+        return min(am, bm)
 
 
 def minlinerastdel(pos):
-    minrast=999999
-    minline=None
+    minrast = 999999
+    minline = None
     for line in lines:
-        if not(minline) or minrast>linedotrast(pos,line):
-            minline=line
-            minrast=linedotrast(pos,line)
+        if not(minline) or minrast>linedotrast(pos, line):
+            minline = line
+            minrast = linedotrast(pos, line)
     if minline:
         lines.remove(minline)
 
@@ -75,28 +75,28 @@ def minlinerastdel(pos):
 pygame.init()
 dotsgen()
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-dot1=(0,0)
-dot2=None
-drag=False
+dot1 = (0, 0)
+dot2 = None
+drag = False
 
 
 while True:
-    screen.fill((200,200,200))
+    screen.fill((200, 200, 200))
     for event in pygame.event.get():
         if event.type == pygame.QUIT: exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button==1:
-                drag=True
+            if event.button == 1:
+                drag = True
                 dot1 = corrpos(event.pos)
                 dot2 = None
-            if event.button==3:
+            if event.button == 3:
                 minlinerastdel(event.pos)
         if drag:
             mousepos = corrpos(event.pos)
-        if event.type == pygame.MOUSEBUTTONUP and event.button==1:
+        if event.type==pygame.MOUSEBUTTONUP and event.button==1:
             drag = False
-            if dot1!=corrpos(mousepos) and (dot1,mousepos) not in lines:
-                lines.append((dot1,mousepos))
+            if dot1!=corrpos(mousepos) and (dot1, mousepos) not in lines:
+                lines.append((dot1, mousepos))
     if drag:
         pygame.draw.line(screen, (100, 100, 100), dot1, mousepos, 5)
     linesdraw()
